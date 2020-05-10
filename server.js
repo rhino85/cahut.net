973c66a05472e4d6015bb9f32f4f7bd5ec45389e
root <root@debian10>
2020-05-10 11:32:58 +0000

    tested master

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ var passport = require('passport');
 var Strategy = require('passport-local').Strategy;
 const jsonfile = require('jsonfile')
 var fs = require('fs');
-
 var db = require('./db');
+var app = express();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
 
 
 // Configure the local strategy for use by Passport.
 
 passport.use(new Strategy(
   function(username, password, done) {
-    console.log("newstrat");
     db.users.findByUsername(username, function(err, user) {
       if (err) { return done(err); }
       if (!user) { return done(null, false); }
@@ -34,21 +37,6 @@ passport.deserializeUser(function(id, cb) {
   });
 });
 
-// Create a new Express application.
-var app = express();
-
-// Configure view engine to render EJS templates.
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.use(function(req, res, next){
-  if (req.originalUrl != req.path){
-    res.redirect(req.path);
-  } else{
-    next();
-  }
-})
-
 // Use application-level middleware for common functionality, including
 // logging, parsing, and session handling.
 app.use(express.json()) // for parsing application/json
@@ -62,57 +50,70 @@ app.use(passport.session());
 
 
 
-var routes = new Object();                            //object that deals with routes
-routes.map = new Map();                               //it got a map to store them
-routes.handler = function(req, res, next){            //now thats a middlewar
-  if(!routes.map.has(req.path)){               //if route doesn't already exist in memory
-  jsonfile.readFile('public/html'+req.path+'.json', function (err, obj) {    // i try to read it from files
-    if (err) {                                                                      // if not in files (i guess)
-      console.error(err)
-      var route = {                                                                 //i create a route object to store in memory
-        url:req.path,
-        markdown:"",
-        html : "",
-        nothing : 1                                                                    //stating it is not modified yet (latter use : no need to look for a file if nothing = 1)
-      };
-      routes.map.set(req.path, route);                             //storing in memory (map object) + populate the req object for next 
-      req.wesh = route;
-    }
-    else {                                                                            //(else) the file already exist ! (i guess)
-      routes.map.set(req.path, obj);                               //storing in memory (map object) + populate the req object for next 
-      req.wesh = obj;
+app.use(function(req, res, next){              //silly middleware to get rid of unwanted querystring (ex : fbclid from fb)
+  console.log('------------------------------------------------');
+  console.log("1 redirect middleware " + req.originalUrl + " " +req.path);
+  if (req.originalUrl == req.path){
+    console.log("1.1 next");
+    next();
+  } else {
+    console.log("1.2 redirected");
+    res.redirect(req.path);
+  }
+})
 
-    }
-    next();                                                                         //ok next
-  });
+
+
+
+
+var routes = new Object();                            //object that deals with routes
+routes.map = new Map();                               //it got a map to store them in memory (so i can avoid useless FS operation)
+routes.handler = function(req, res, next){            //now thats a middlewar that i use on "get" request
+  console.log("3 routehandler " + req.originalUrl);
+  if(!routes.map.has(req.originalUrl)){                                   //if route doesn't already exist in map
+    console.log("3.1 not in map " + req.originalUrl);
+    jsonfile.readFile('public/html'+req.path+'.json', function (err, fileContent) {    // i try to read it from files
+      if (err) {                                                                  // if not in files
+        console.log("3.1.1 nor in file " + req.originalUrl);
+        var route = {                                                                //i create a route object to store in map
+          url:req.originalUrl,
+          markdown:"",
+          html : "",
+          nothing : 1                                                                //stating it is not modified yet (latter use : no need to look for a file if nothing = 1)
+        };
+        routes.map.set(req.path, route);                                             //storing in map object 
+        req.data = route;                                                            //populate the res object for next 
+      }
+      else {                                                                      //(else) the file already exist but not in map object
+        console.log("3.1.2 but in file " + req.originalUrl);
+        routes.map.set(req.path, fileContent);                                       //storing in map object 
+        req.data = fileContent;                                                      //populate the res object for next
+      }
+      next();
+    });
   }
   else {                                                                            //if route already registred in memory
-    routes.map.get(req.path);
-    req.wesh = routes.map.get(req.path);
-
-    next();
+    console.log("3.2 it's already in map " + req.originalUrl);
+    req.data = routes.map.get(req.originalUrl);    
+    next();                                 
   }
 
 }
 
-app.use(express.static('public'))
-app.use(routes.handler);
+let prefix = "";           //not sure this will stay in the future, probably better solution to find
 
-let prefix = "";
-// Define routes.
-
-  app.post(prefix+'/login', function(req, res, next) {
+  app.post(prefix+'/login', function(req, res, next) {          // i need to work on this
+    console.log("login post");
     
     passport.authenticate('local', function(error, user, info) {
-      console.log("authenticate");
       if(error) {
         res.status(500).json(error);
       }
       else if(!user) {
-        res.status(401).json(info.message);
+        res.status(401).json(info);
       } else {
         req.login(user, function(err){
-          console.log("req.login");
+          console.log("logged in");
 
           res.json(user);
         })
@@ -120,20 +121,20 @@ let prefix = "";
     })(req, res, next);
   });
   
-app.get(prefix+'/logout',
+app.get(prefix+'/logout',    // i need to work on this
   function(req, res){
     req.logout();
     res.redirect(prefix+'/');
   });
 
-  app.get(prefix+'/*', function(req, res){
-    console.log("test" + req.path);
-    console.dir(req.wesh);
+  app.get(prefix+'/*', routes.handler, function(req, res){
+    console.log("4 get* " + req.originalUrl);
+    //console.dir("req.data : " + req.data);
     if(req.user){
-        res.render('base', {a:{ user: req.user.username, prefix:prefix, data:req.wesh }});
+        res.render('base', {a:{ user: req.user.username, prefix:prefix, data:req.data }});
       }  
       else {
-        res.render('base', {a:{user: 0, prefix:prefix, data:req.wesh }});
+        res.render('base', {a:{user: 0, prefix:prefix, data:req.data }});
       }
 
   });
@@ -144,10 +145,9 @@ app.get(prefix+'/logout',
     if(req.user){     
     
       routes.map.get(req.path).markdown = req.body.markdown;           //storing data in memory
-      routes.map.get(req.path).html = req.body.html;                
-      console.log(req.path);
+      routes.map.get(req.path).html = req.body.html;    
+      console.log("4 post* " + req.originalUrl);
       let dir = 'public/html'+ req.path.slice(0, req.path.lastIndexOf('/'));          // path of the directory if any
-      console.log(dir);
       fs.mkdir(dir, { recursive: true },                                                            //create dir 
         (err) => { 
           if (err) { 
@@ -159,7 +159,6 @@ app.get(prefix+'/logout',
               res.send("error saving");
             }
             else {
-              console.log('saved');
               res.send("saved");
             }
           })
@@ -168,4 +167,4 @@ app.get(prefix+'/logout',
   });
   
 
-app.listen(3000);
+app.listen(3001);
